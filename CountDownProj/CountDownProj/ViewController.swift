a70ee2a0a5ff7a4f711fa9b13a2bce22abca33e6
BleuxGenius <nicoleblack360@gmail.com>
2019-09-23 15:18:56 -0400

    Add files via upload

---
+++ CountDownProj/CountDownProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountDownProj
+//
+//  Created by Lambda_School_Loaner_167 on 9/23/19.
+//  Copyright © 2019 Dani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
